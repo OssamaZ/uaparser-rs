223c36edbbaca6b0e940bee8b156eef2398f82aa
Ossama Zaid <w4rz4zi@gmail.com>
2023-11-26 15:40:10 +0100

    Add user agent tests from uapcore repo

--- src/lib.rs
+++ src/lib.rs
@@ -24,10 +24,64 @@ mod user_agent;
 mod yaml_file;
 
 pub use parser::{Client, UAParser};
+pub use user_agent::UserAgent;
 
 #[cfg(test)]
 mod test {
   use super::*;
+  use serde::Deserialize;
+  use std::fs;
+
+  #[test]
+  fn ua_tests() {
+    #[derive(Deserialize, Debug)]
+    struct UATestFile {
+      test_cases: Vec<UATest>,
+    }
+
+    #[derive(Deserialize, Debug)]
+    struct UATest {
+      #[serde(rename = "user_agent_string")]
+      ua_str: String,
+      family: String,
+      major: Option<String>,
+      minor: Option<String>,
+      patch: Option<String>,
+      patch_minor: Option<String>,
+    }
+
+    let uap = UAParser::from_yaml("./src/regexes.yaml").unwrap();
+    let tests_file =
+      fs::File::open("./src/tests/ua_tests.yaml").expect("Unable to open the ua test file.");
+    let parsed_tests: UATestFile =
+      serde_yaml::from_reader(tests_file).expect("Unable to parse ua test file.");
+
+    let failures: Vec<_> = parsed_tests
+      .test_cases
+      .iter()
+      .map_while(|uat| {
+        let Client { user_agent } = uap.parse(uat.ua_str.clone());
+        let UserAgent {
+          family,
+          major,
+          minor,
+          patch,
+          patch_minor,
+        } = user_agent;
+        if family == uat.family
+          && major == uat.major
+          && minor == uat.minor
+          && patch == uat.patch
+          && patch_minor == uat.patch_minor
+        {
+          return None;
+        }
+        Some(uat.ua_str.as_str())
+      })
+      .collect();
+
+    assert!(failures.is_empty());
+  }
 
   #[test]
   fn test_1() {
