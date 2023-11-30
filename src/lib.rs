283b7d82a5609fedea804e9c66aae7b879862d86
Ossama Zaid <w4rz4zi@gmail.com>
2023-11-30 07:48:31 +0100

    Add device tests

--- src/lib.rs
+++ src/lib.rs
@@ -74,39 +74,6 @@ mod test {
   use std::fs;
 
   #[test]
-  fn test_ua() {
-    let uap = UAParser::from_yaml("./src/tests/regexes.yaml").unwrap();
-    let ua_str = String::from("Mozilla/5.0 (Linux; Android 4.0.1; Galaxy Nexus Build/ITL41F) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.58 Mobile Safari/537.31");
-    let client = uap.parse(ua_str);
-    assert_eq!(client.user_agent.family, "Chrome Mobile");
-    assert_eq!(client.user_agent.major, Some(String::from("26")));
-    assert_eq!(client.user_agent.minor, Some(String::from("0")));
-    assert_eq!(client.user_agent.patch, Some(String::from("1410")));
-    assert_eq!(client.user_agent.patch_minor, Some(String::from("58")));
-  }
-
-  #[test]
-  fn test_os() {
-    let uap = UAParser::from_yaml("./src/tests/regexes.yaml").unwrap();
-    let ua_str = String::from("Mozilla/5.0 (Linux; Android 4.0.1; Galaxy Nexus Build/ITL41F) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.58 Mobile Safari/537.31");
-    let client = uap.parse(ua_str);
-    assert_eq!(client.os.family, "Android");
-    assert_eq!(client.os.major, Some(String::from("4")));
-    assert_eq!(client.os.minor, Some(String::from("0")));
-    assert_eq!(client.os.patch, Some(String::from("1")));
-  }
-
-  #[test]
-  fn test_device() {
-    let uap = UAParser::from_yaml("./src/tests/regexes.yaml").unwrap();
-    let ua_str = String::from("Mozilla/5.0 (Linux; Android 4.0.1; Galaxy Nexus Build/ITL41F) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.58 Mobile Safari/537.31");
-    let client = uap.parse(ua_str);
-    assert_eq!(client.device.family, "Samsung Galaxy Nexus");
-    assert_eq!(client.device.brand, Some(String::from("Samsung")));
-    assert_eq!(client.device.model, Some(String::from("Galaxy Nexus")));
-  }
-
-  #[test]
   fn ua_tests() {
     #[derive(Deserialize, Debug)]
     struct UATestFile {
@@ -135,21 +102,17 @@ mod test {
       .iter()
       .map_while(|uat| {
         let Client { user_agent, .. } = uap.parse(uat.ua_str.clone());
-        let UserAgent {
-          family,
-          major,
-          minor,
-          patch,
-          patch_minor,
-        } = user_agent;
-        if family == uat.family
-          && major == uat.major
-          && minor == uat.minor
-          && patch == uat.patch
-          && patch_minor == uat.patch_minor
+        if user_agent.family == uat.family
+          && user_agent.major == uat.major
+          && user_agent.minor == uat.minor
+          && user_agent.patch == uat.patch
+          && user_agent.patch_minor == uat.patch_minor
         {
           return None;
         }
+        println!("Expected: {:#?}", uat);
+        println!("Got: {:#?}", user_agent);
+        println!("--");
         Some(uat.ua_str.as_str())
       })
       .collect();
@@ -186,13 +149,6 @@ mod test {
       .iter()
       .map_while(|uat| {
         let Client { os, .. } = uap.parse(uat.ua_str.clone());
-        // let Os {
-        //   family,
-        //   major,
-        //   minor,
-        //   patch,
-        //   patch_minor,
-        // } = os;
         if os.family == uat.family
           && os.major == uat.major
           && os.minor == uat.minor
@@ -203,6 +159,46 @@ mod test {
         }
         println!("Expected: {:#?}", uat);
         println!("Got: {:#?}", os);
+        println!("--");
+        Some(uat.ua_str.as_str())
+      })
+      .collect();
+
+    assert!(failures.is_empty());
+  }
+
+  #[test]
+  fn device_tests() {
+    #[derive(Deserialize, Debug)]
+    struct DeviceTestFile {
+      test_cases: Vec<DeviceTest>,
+    }
+
+    #[derive(Deserialize, Debug)]
+    struct DeviceTest {
+      #[serde(rename = "user_agent_string")]
+      ua_str: String,
+      family: String,
+      brand: Option<String>,
+      model: Option<String>,
+    }
+
+    let uap = UAParser::from_yaml("./src/tests/regexes.yaml").unwrap();
+    let tests_file =
+      fs::File::open("./src/tests/device_tests.yaml").expect("Unable to open the ua test file.");
+    let parsed_tests: DeviceTestFile =
+      serde_yaml::from_reader(tests_file).expect("Unable to parse ua test file.");
+
+    let failures: Vec<_> = parsed_tests
+      .test_cases
+      .iter()
+      .map_while(|uat| {
+        let Client { device, .. } = uap.parse(uat.ua_str.clone());
+        if device.family == uat.family && device.brand == uat.brand && device.model == uat.model {
+          return None;
+        }
+        println!("Expected: {:#?}", uat);
+        println!("Got: {:#?}", device);
         println!("--");
         Some(uat.ua_str.as_str())
       })
